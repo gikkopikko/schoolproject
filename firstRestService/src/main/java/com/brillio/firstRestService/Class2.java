93c25c99b42c2502c9de8888ee07e2dcccc0ff13
unknown <4ssiddharth@gmail.com>
2021-09-14 12:51:41 +0530

    web hook try 3

--- firstRestService/src/main/java/com/brillio/firstRestService/Class2.java
+++ firstRestService/src/main/java/com/brillio/firstRestService/Class2.java
@@ -4,7 +4,7 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("WEB HOOK TRY 3");
+		System.out.println("WEB HOOK TRY 4");
 
 	}
 
